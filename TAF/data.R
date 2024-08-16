f92a22bde2532581dc3cab71efe103694b009c41
Arni Magnusson <thisisarni@gmail.com>
2024-08-16 17:15:38 +0800

    Preprocess data, write TAF data tables

--- TAF/data.R
+++ TAF/data.R
@@ -1,9 +1,51 @@
 ## Preprocess data, write TAF data tables
 
-## Before:
-## After:
+## Before: fdesc.txt, alb.age_length, alb.frq (boot/data),
+##         length.fit (boot/model_results)
+## After:  cpue.csv, fisheries.csv, length_comps.csv, otoliths.csv (data)
 
 library(TAF)
+taf.library(FLR4MFCL)
+source("utilities.R")  # reading
 
 mkdir("data")
 
+# Read data
+oto <- reading("otolith data",
+               read.MFCLALK("boot/data/alb.age_length",
+                            "boot/data/model_results/length.fit"))
+frq <- reading("catch data", read.MFCLFrq("boot/data/alb.frq"))
+fisheries <- reading("fisheries description",
+                     read.table("boot/data/fdesc.txt", fill=TRUE, header=TRUE))
+
+# Fisheries description
+names(fisheries)[names(fisheries) == "num"] <- "fishery"
+names(fisheries)[names(fisheries) == "region"] <- "area"
+
+# Otolith data
+otoliths <- ALK(oto)
+otoliths <- otoliths[otoliths$obs > 0,]
+otoliths <- otoliths[rep(seq_len(nrow(otoliths)), otoliths$obs),]
+otoliths$area <- fisheries$area[otoliths$fishery]
+otoliths <- otoliths[c("year", "month", "area", "age", "length")]
+
+# CPUE data
+cpue <- realisations(frq)
+cpue <- cpue[cpue$fishery %in% 18:20,]  # index fisheries
+cpue <- merge(cpue, fisheries[c("fishery", "area")])
+cpue$index <- cpue$catch / cpue$effort
+cpue <- cpue[c("year", "month", "fishery", "area", "index")]
+
+# Size data
+size <- freq(frq)
+size <- size[size$freq != -1,]
+
+# Length compositions
+length.comps <- size[!is.na(size$length),]
+length.comps <- length.comps[c("year", "month", "fishery", "length", "freq")]
+
+# Write TAF tables
+write.taf(fisheries, dir="data")
+write.taf(otoliths, dir="data")
+write.taf(cpue, dir="data")
+write.taf(length.comps, dir="data")
