10fea021bebf1e4568e14ac1c4f572a17fea919d
Arni Magnusson <thisisarni@gmail.com>
2024-08-19 15:04:39 +0800

    Run analysis, write model results

--- TAF/model.R
+++ TAF/model.R
@@ -1,9 +1,24 @@
 ## Run analysis, write model results
 
-## Before:
+## Before: 08.par, alb.age_length, alb.frq, doitall.sh, mfcl.cfg (boot/data),
+##         mfclo64 (boot/software)
 ## After:
 
 library(TAF)
 
 mkdir("model")
 
+# Software
+cp("boot/software/mfclo64", "model")
+
+# Input files
+cp("boot/data/08.par",         "model")
+cp("boot/data/alb.age_length", "model")
+cp("boot/data/alb.frq",        "model")
+cp("boot/data/doitall.sh",     "model")
+cp("boot/data/mfcl.cfg",       "model")
+
+# Run model
+setwd("model")
+system("doitall.sh")
+setwd("..")
